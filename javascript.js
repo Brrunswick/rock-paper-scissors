3e16d2b4d0e965ac971aae56428017dca3621c98
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-17 13:12:55 +0100

    wrote javascript to allow a single round to be played in the console.

--- javascript.js
+++ javascript.js
@@ -13,13 +13,26 @@ function computerChoice() {
         return "scissors"
     }
 }
-
-computerChoice();
-
 let playerChoice = (prompt("Rock, Paper or Scissors?"));
 
-if (playerChoice.toLowerCase() === "rock" || playerChoice.toLowerCase() === "paper" || playerChoice.toLowerCase() === "scissors") {
-    console.log("thanks")
-} else {
-    console.log ("please choose rock paper or scissors")
+if (playerChoice.toLowerCase() != "rock" && playerChoice.toLowerCase() != "paper" && playerChoice.toLowerCase() != "scissors") {
+    console.log("please choose rock paper or scissors")
+} 
+
+function playRound(playerChoice, computerChoice) {
+    console.log(`the computer chose ${computerChoice}`)
+    console.log(`You picked ${playerChoice.toLowerCase()}`)
+    if (playerChoice.toLowerCase() === computerChoice) {
+        console.log("draw")
+    } else if ((playerChoice.toLowerCase() === "rock" && computerChoice === "paper") || (playerChoice.toLowerCase() === "paper" && computerChoice === "scissors") || (playerChoice.toLowerCase() === "scissors" && computerChoice === "rock")) {
+        console.log("the computer wins")
+        computerScore += 1
+    } else {
+        console.log("You win")
+        playerScore += 1
+    }
+    
+    console.log(`the score is ${playerScore} - ${computerScore}`)
 }
+
+playRound(playerChoice, computerChoice());
